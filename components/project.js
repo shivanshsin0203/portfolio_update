326890363c49ae9f106513069429ff0252489c52
Shivansh Singh <singhshivansh12may@gmail.com>
2024-12-10 10:56:10 +0530

    new project added

--- components/project.js
+++ components/project.js
@@ -11,7 +11,17 @@ export const getProjects = () => {
     "demos":"https://x.com/ShivanshSi0203/status/1769718417060167938"
   },
   {
-    "id": 2,
+      "id":2,
+      "name":"2d Metaverse",
+      "image":"https://miro.medium.com/v2/resize:fit:1400/0*EkBM0pKrO7FPwV2Z",
+      "description":"A 2D metaverse platform that leverages WebSockets for real-time communication and multiplayer functionality with WEBRTC. The platform features a custom physics engine for interactive gameplay and a Node.js backend for server-side logic. Built with TypeScript, React, and Socket.IO, it enables users to create and join virtual rooms, interact with other players, and enjoy a seamless multiplayer experience.",
+      "tags":["typescript","react","socket.io","nodejs","webrtc","cloudfare","gcp"],
+      "code":"https://github.com/shivanshsin0203/2d_metaverse",
+      "demo":"https://2dmetaverse.pages.dev",
+      "demos":"https://x.com/ShivanshSi0203/status/1866139270542410019"
+  },
+  {
+    "id": 3,
     "name": "Creedo (Social App)",
     "image": "https://media.licdn.com/dms/image/v2/D5622AQFddrpzVxrvuQ/feedshare-shrink_800/feedshare-shrink_800/0/1707224146862?e=1732147200&v=beta&t=kJXekZaCpiys6abbcqKSQZ-ErE3VopExSA7pZTGYlxQ",
     "description": "Node Js and Socket.Io for backend and real-timenotification. Kinde for session and authentication. Use of SSR and CSR in Next js. React Dropzone for drag and drop.MongoDB for the database. Cloudinary for cloud storage ",
@@ -21,7 +31,7 @@ export const getProjects = () => {
     "demos":"https://x.com/ShivanshSi0203/status/1779383162684227895"
   },
   {
-    "id": 3,
+    "id": 4,
     "name": "Ai ArchiText",
     "image": "https://pbs.twimg.com/media/GaL_accbUAETWT0?format=jpg&name=medium",
     "description": "A online website generator which uses chatgpt API and Dalle to make deployed websites. ",
@@ -32,7 +42,7 @@ export const getProjects = () => {
 
   },
    {
-    "id": 4,
+    "id": 5,
     "name": "Repl",
     "image": "https://pbs.twimg.com/media/GaL_xN7bUAMNT9V?format=jpg&name=medium",
     "description": "REPL is an innovative online development environment similar to Replit, offering secure user authentication via KindAuth and a Next.js frontend. It features Docker-based isolated project environments, real-time WebSocket connections, and an integrated code editor with node-pty pseudo-terminals for seamless development. Built with TypeScript, Node.js, and MongoDB, it enables users to host Express servers or React apps in scalable, isolated environments",
@@ -42,7 +52,7 @@ export const getProjects = () => {
     "demos":"https://x.com/ShivanshSi0203/status/1812522400556126696"
   },
   {
-    "id": 5,
+    "id": 6,
     "name": "File Manager (Routines)",
     "image": "https://img.freepik.com/premium-vector/upload-files-form-with-submit-button-dark-ui-design_116137-2354.jpg",
     "description": "A sophisticated video streaming platform leveraging Go and HLS (HTTP Live Streaming) technology, featuring robust video processing capabilities and AWS S3 integration for efficient content delivery. The system implements parallel processing through Go routines and Redis-managed queues, enabling simultaneous video transcoding and optimized performance for multiple file handling. Built with scalability in mind, the platform utilizes FFmpeg for HLS format conversion, creating adaptive bitrate streaming solutions while efficiently managing system resources through a well-orchestrated worker pool system",
@@ -52,7 +62,7 @@ export const getProjects = () => {
     "demos":"https://x.com/ShivanshSi0203/status/1844335229500362784"
   },
   {
-    "id": 6,
+    "id": 7,
     "name": "PlantCare APP",
     "image": "https://mir-s3-cdn-cf.behance.net/projects/404/075b6d208373693.Y3JvcCwyNDA3LDE4ODIsMTI1LDk1.png",
     "description": "An innovative plant care application powered by Google's Gemini API that provides instant plant identification and personalized care recommendations through AI technology. Leveraging Expo for the frontend and Node.js for the backend, the app delivers a seamless user experience with robust image processing capabilities.",
@@ -62,7 +72,7 @@ export const getProjects = () => {
     "demos":"https://x.com/ShivanshSi0203/status/1822508040329007111"
   },
   {
-    "id": 7,
+    "id": 8,
     "name": "Task It APP",
     "image": "https://i0.wp.com/9to5google.com/wp-content/uploads/sites/4/2021/09/google-tasks-logo-new.jpeg?resize=1200%2C628&quality=82&strip=all&ssl=1",
     "description": "A cutting-edge ToDo application built with React Native Expo that seamlessly combines functionality with aesthetic appeal. This feature-rich task manager offers scheduled local notifications, priority-based reminders, and offline storage capabilities, ensuring users never miss important tasks. The app stands out with its fluid interface enhanced by Lottie animations and React Native's native animations",
